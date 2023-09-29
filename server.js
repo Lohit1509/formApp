2b8bad9b88683c19f8405688cfc67b0cfcd4590b
Lohit1509 <98259803+Lohit1509@users.noreply.github.com>
2023-09-30 01:15:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ app.post('/login-user', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}...`);
 });
